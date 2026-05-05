a5fd99eddb4c9648ff6e4d200d0ef859f0265f12
jacquesdoc <jacquesmariedrigbe@gmail.com>
2026-05-05 22:21:22 +0000

    Correction CORS et DashboardController

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,9 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        //
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->statefulApi();
+
+        // CORS pour toutes les routes API
+        $middleware->prepend(\Illuminate\Http\Middleware\HandleCors::class);
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
